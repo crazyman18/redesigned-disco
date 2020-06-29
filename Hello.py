84bdae5bcb031e0e595408ae71b93a7c0bf91274
crazyman18 <65572083+crazyman18@users.noreply.github.com>
2020-06-30 10:11:34 +1200

    Add files via upload

--- Hello.py
+++ Hello.py
@@ -1,1 +1,20 @@
-print (Hello, Samuel!)
+print ("Hello, Samuel!")
+
+print ("""How are you...
+Are you Fine...
+Are you Good..""")
+
+print (5, 3)
+print (5 + 3)
+print (5 - 3)
+print (5 / 3)
+print (5 * 3)
+
+print ("Hello" + "Samuel")
+print ("HelloSamuel" * 3)
+
+name = input("What is your name?")
+print(name)
+age = input("What is your age?")
+print(age)
+print (name, age)
