11e7b421ad9568f06dcd4fd6dbc8038923b639b1
crazyman18 <65572083+crazyman18@users.noreply.github.com>
2020-07-24 14:08:03 +1200

    Add files via upload

---
+++ Phyton 1.py
@@ -0,0 +1,32 @@
+print("\"Hello, Samuel!\"")
+
+print(3)
+print(5 + 3)
+print(8 / 2)
+print(12 * 6)
+
+name = input("What is your name?")
+print(name)
+
+#you can use the format one samuel if you want to place aposthrope
+#or the other one.
+name = input("What is your name?")
+print("Hello  " + name )
+print(name + " is a nice name!")
+print("\"{} is a nice name!\"".format(name))
+print('Python was named after "Monty Python"')
+print("And the computer said: \"I'm a computer, silly!\"")
+
+print("Samuel")
+print("Monty Python " + "and the Holy Grail")
+print("Spam!" * 8)
+
+print("Look, that rabbit's got a vicious streak a mile wide! It's a killer!")
+print('We are no longer the knights who say "ni", we are now the knights who say "ekki-ekki-ekki-pitang-zoom-boing!"')
+print("There are 10 types of people in this world, those who know \"binary\" and those who don\'t.")
+
+print("""Hitting enter here
+does work!""")
+print("Here is text \n on two lines")
+print("Here is a multi-line \n"
+"print statement")
