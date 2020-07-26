aa78202c104bf0dd8ee53b43cdf07e486c08a5b4
crazyman18 <65572083+crazyman18@users.noreply.github.com>
2020-07-27 09:12:49 +1200

    Add files via upload

--- Phyton 1.py
+++ Phyton 1.py
@@ -30,3 +30,49 @@ does work!""")
 print("Here is text \n on two lines")
 print("Here is a multi-line \n"
 "print statement")
+
+poem = """When you're chewing on life's gristle
+Don't grumble, give a whistle
+And this'll help things turn out for the best...
+And...always look on the bright side of life... 
+Always look on the light side of life..."""
+print(poem)
+
+phrase = "Today you are feeling: \n"
+
+mood = "Happy"
+print(mood)
+
+mood = input("How are you feeling today")
+print(mood)
+print(phrase + mood)
+
+print(2 * 5 + 3)
+#exponent we have BEMDAS operators
+print(5 ** 4)
+print(18 - 6)
+
+#means first the brackets then next operation
+print((7 + 3) / 2)
+print(20 - 2 ** 2)
+print((5 + 2) * 10)
+
+#integers and floats
+result_1 = 8 / 2
+result_2 = 3 + 8
+result_3 = 0.1 + 0.2
+result_4 = 0.1 + 0.7
+result_5 = 0.5 + 1
+print(result_1)
+print(result_2)
+print(result_3)
+print(result_4)
+print(result_5)
+
+#Create variables
+hourly_rate = 15.50
+hours_worked = 40
+wages = 15.50 * 40
+#Calculate wages based on hourly_rate and hours_worked
+print(wages)
+#Print the result
